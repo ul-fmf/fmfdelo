668fd1728f487bb22ce5a7b44d0f07f552599dae
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2023-02-19 13:14:32 +0100

    doda flatpak ppa

--- pripravi-arhive.sh
+++ pripravi-arhive.sh
@@ -2,6 +2,8 @@
 set -e
 
 echo "Installing veraPDF"
+add-apt-repository ppa:flatpak/stable
+apt update
 apt-get install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --noninteractive flathub org.verapdf.veraPDF
